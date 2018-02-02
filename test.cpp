020809dac6bb099a996e77b37e509e22248256ac
Unknown <daniena@stud.ntnu.no>
2018-02-02 12:51:21 +0100

    Add psuedocode for fitness function

--- test.cpp
+++ test.cpp
@@ -1,25 +1,44 @@
 #include<iostream>
+#include<cmath>
 #include"decode.h"
 
 bool test_ONOFF = true;
 
-double placeholder_fitness(Individual ind) {
+double placeholder_fitness_individual(Individual ind) {
     double fitness = 0;
-    int i  = 0;
+    double duration = 0;
+    double load = 0;
+    double punishment = 0;
 
-    city c = get_city(0);
+    city c = ind.get_city(0);
     city cn = c;
 
-    fitness += (c.x-ind.d.x)^2 + (c.y-ind.d.y)^2;
-
-    while( i++ < n_customers-1) {
+    duration += sqrt((c.x-ind.d.x)^2 + (c.y-ind.d.y)^2);
+    for(int i = 0; i < n_customers-1; i++) {
         c = cn;
-        cn = get_city(i+1);
-        fitness += (cn.x-c.x)^2 + (cn.y - c.y)^2;
+        cn = ind.get_city(i+1);
+        duration += sqrt((cn.x-c.x)^2 + (cn.y - c.y)^2);
+        duration += c.duration;
+        load += c.load;
     }
+    duration += sqrt((cn.x-ind.d.x)^2 + (cn.y-ind.d.y)^2);
+    load += cn.load;
 
+    if(duration > ind.d.max_duration_per_vehicle) punishment += 500;
+    if(load > ind.d.max_load_per_vehicle) punishment += 500;
+    fitness = duration + punishment;
+
+    return fitness;
+}
+
+double placeholder_fitness_total(Population pop) { //Blir vel i så fall en litt meningsløs funksjon.
+    double fitness;
+    for each individual in population:
+        fitness = placeholder_fitness_individual(individual);
     
-
+    if not all cities visited: // Dette er vel umulig i utgangspunktet
+        cout << "Illegal configuration" << endl;
+        exit(1);
 }
 
 int main(){
