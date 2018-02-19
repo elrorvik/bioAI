eae5b40b67ac8ff0afd670311c85cd41beb6f7f8
Unknown <daniena@stud.ntnu.no>
2018-02-19 16:22:32 +0100

    Add node and RGB

---
+++ MOEA/MOEA/population.cpp
@@ -0,0 +1,10 @@
+#include"population.h"
+
+Population::~Population() {
+
+}
+
+void Population::MST_Kruskal() {
+	// Remove the two nodes with least edge weight (weight = "cost" = RGB distance) and add it to its own tree
+	
+}
--- MOEA/MOEA/population.h
+++ MOEA/MOEA/population.h
@@ -1,1 +1,21 @@
 #pragma once
+#include<vector>
+
+struct RGB {
+	int R;
+	int G;
+	int B;
+};
+
+enum node { self, up, down, left, right };
+
+class Population
+{
+private:
+	RGB **image;
+	node ***population;
+public:
+	Population() {};
+	~Population();
+	void MST_Kruskal();
+};
