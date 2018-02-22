861f2d5eb2911e8160096a5d1cbf1a0c7c90fa8d
Unknown <daniena@stud.ntnu.no>
2018-02-22 19:56:04 +0100

    Add pos and node operators

---
+++ MOEA/MOEA/global.cpp
@@ -0,0 +1,59 @@
+#include"global.h"
+
+pos operator+(pos coord, direction dir) {
+switch (dir) {
+case SELF:
+	return coord;
+case UP:
+	coord.y -= 1;
+	return coord;
+case DOWN:
+	coord.y += 1;
+	return coord;
+case LEFT:
+	coord.x -= 1;
+	return coord;
+case RIGHT:
+	coord.x += 1;
+	return coord;
+};
+return coord;
+};
+
+void operator+(node &n, direction d) {
+	switch (d) {
+	case SELF:
+		return;
+	case UP:
+		n.up = true;
+		return;
+	case DOWN:
+		n.down = true;
+		return;
+	case LEFT:
+		n.left = true;
+		return;
+	case RIGHT:
+		n.right = true;
+		return;
+	}
+}
+
+void operator-(node &n, direction d) {
+	switch (d) {
+	case SELF:
+		return;
+	case UP:
+		n.up = false;
+		return;
+	case DOWN:
+		n.down = false;
+		return;
+	case LEFT:
+		n.left = false;
+		return;
+	case RIGHT:
+		n.right = false;
+		return;
+	}
+}
