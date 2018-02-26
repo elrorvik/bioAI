b63adeff65de4033b1ec4a1abb28007196794d2e
Unknown <daniena@stud.ntnu.no>
2018-02-26 19:29:56 +0100

    Add NSGAII

---
+++ MOEA/MOEA/NSGAII.cpp
@@ -0,0 +1,27 @@
+#include"NSGAII.h"
+#include"population.h"
+#include"fitness.h"
+#include"global.h"
+#include<utility>
+#include<algorithm>
+
+std::vector<int> NSGAII(Population &p, const std::vector<pos> * entry_s) {
+	
+	int rank[N_IND]{}; // TODO: Make sure N_IND is the number of parents + children in this case
+	double fitness_1[N_IND]{};
+	double fitness_2[N_IND]{};
+
+	// Non dominated sorting
+	for (int ind_index = 0; ind_index < N_IND; ind_index++) { // TODO: Make sure N_IND is the number of parents + children in this case
+		fitness_1[ind_index] = overall_deviation_ind(p, ind_index, entry_s[ind_index]);
+		fitness_2[ind_index] = edge_value_ind(p, ind_index, entry_s[ind_index]);
+	}
+
+	for (int ind_index = 0; ind_index < N_IND; ind_index++) {
+		for (int other_index = 0; other_index < N_IND; other_index++) {
+			if (fitness_1[ind_index] > fitness_1[other_index] && fitness_2[ind_index] > fitness_2[other_index]) rank[ind_index]++;
+		}
+	}
+
+
+}
---
+++ MOEA/MOEA/NSGAII.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef NSGAII_H
+#define NSGAII_H
+#include<vector>
+
+struct Population;
+std::vector<int> NSGAII(Population &p);
+
+#endif
