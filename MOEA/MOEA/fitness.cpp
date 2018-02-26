63c947f4cdee91a5b6e813153fb263f183bb7694
Unknown <daniena@stud.ntnu.no>
2018-02-26 15:31:15 +0100

    Fix overall_deviation

--- MOEA/MOEA/fitness.cpp
+++ MOEA/MOEA/fitness.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include "global.h"
 #include "fitness.h"
+#include"population.h"
 
 
 
@@ -9,6 +10,14 @@ double dist(RGB p1, RGB p2) {
 	return sqrt(pow(p1.r - p2.r, 2) + pow(p1.g - p2.g, 2) + pow(p1.b - p2.b, 2));
 }
 
-void find_pixels_segment(int x, int y) {
+double overall_deviation(Population &p, int ind_index, node s_entry) {
+	std::stack<pos> branch_points;
+	pos pos_i;
+	pos_i = p.traverse_ST(ind_index, s_entry.entry, branch_points);
+}
 
+
+
+void find_pixels_segment(int x, int y) {
+	
 }
